2982ab9227cc1cc63fcd40f6cebca186f5648320
BobbyM04 <156210437+BobbyM04@users.noreply.github.com>
2025-01-14 13:58:09 -0500

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,17 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerController : MonoBehaviour{
 
-    // Update is called once per frame
-    void Update()
-    {
+    public GameObject InGameMenuCanvas;
+    public GameObject InGameMenuPanel;
+
+    private bool isMenuActive = false;
+
+    private void Start(){
         
+        // Ensure the in-game menu is initially inactive
+        if(InGameMenuCanvas != null){
+
+            InGameMenuCanvas.SetActive(false);
+        }else{
+
+            Debug.LogError("In-Game Menu Canvas is not assigned in the inspector.");
+        }
+        if (InGameMenuPanel != null){
+
+            InGameMenuPanel.SetActive(false);
+        }else{
+
+            Debug.LogError("In-Game Menu Panel is not assigned in the inspector.");
+        }
+    }
+    private void Update(){
+
+        // Check if the "Tab" key is pressed
+        if (Input.GetKeyDown(KeyCode.Tab)){
+
+            ToggleInGameMenu();
+        }
+    }
+    private void ToggleInGameMenu(){
+
+        isMenuActive = !isMenuActive;
+
+        if (isMenuActive){
+
+            if(InGameMenuCanvas != null){
+
+                InGameMenuCanvas.SetActive(true);
+            }
+            if (InGameMenuPanel != null){
+
+                InGameMenuPanel.SetActive(true);
+            }
+            Debug.Log("In-Game Menu activated.");
+        }else{
+
+            if (InGameMenuCanvas != null){
+
+                InGameMenuCanvas.SetActive(false);
+            }
+            if (InGameMenuPanel != null){
+
+                InGameMenuPanel.SetActive(false);
+            }
+            Debug.Log("In-Game Menu activated.");
+        }
     }
 }
